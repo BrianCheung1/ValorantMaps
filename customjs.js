f2a62ca464d7871108b74c325b8989d586ee85fa
Brian Cheung <brian.cheung1@baruchmail.cuny.edu>
2021-02-17 23:48:29 -0500

    added gun filters

---
+++ customjs.js
@@ -0,0 +1,76 @@
+$(document).ready(function () {
+    $("#btn-all").click(function () {
+        $(".sidearms").show();
+        $(".smgs").show();
+        $(".shotguns").show();
+        $(".rifles").show();
+        $(".snipers").show();
+        $(".heavys").show();
+    });
+});
+
+$(document).ready(function () {
+    $("#btn-sidearms").click(function () {
+        $(".sidearms").show();
+        $(".smgs").hide();
+        $(".shotguns").hide();
+        $(".rifles").hide();
+        $(".snipers").hide();
+        $(".heavys").hide();
+    });
+});
+
+$(document).ready(function () {
+    $("#btn-smgs").click(function () {
+        $(".sidearms").hide();
+        $(".smgs").show();
+        $(".shotguns").hide();
+        $(".rifles").hide();
+        $(".snipers").hide();
+        $(".heavys").hide();
+    });
+});
+
+$(document).ready(function () {
+    $("#btn-shotguns").click(function () {
+        $(".sidearms").hide();
+        $(".smgs").hide();
+        $(".shotguns").show();
+        $(".rifles").hide();
+        $(".snipers").hide();
+        $(".heavys").hide();
+    });
+});
+
+$(document).ready(function () {
+    $("#btn-rifles").click(function () {
+        $(".sidearms").hide();
+        $(".smgs").hide();
+        $(".shotguns").hide();
+        $(".rifles").show();
+        $(".snipers").hide();
+        $(".heavys").hide();
+    });
+});
+
+$(document).ready(function () {
+    $("#btn-snipers").click(function () {
+        $(".sidearms").hide();
+        $(".smgs").hide();
+        $(".shotguns").hide();
+        $(".rifles").hide();
+        $(".snipers").show();
+        $(".heavys").hide();
+    });
+});
+
+$(document).ready(function () {
+    $("#btn-heavys").click(function () {
+        $(".sidearms").hide();
+        $(".smgs").hide();
+        $(".shotguns").hide();
+        $(".rifles").hide();
+        $(".snipers").hide();
+        $(".heavys").show();
+    });
+});
