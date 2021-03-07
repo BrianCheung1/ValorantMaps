3cab67fab2c8db234adbdd09871fd2e0a1cb2501
Brian Cheung <brian.cheung1@baruchmail.cuny.edu>
2021-03-07 13:47:13 -0500

    fixed scroll bar overflow and button changes

--- customjs.js
+++ customjs.js
@@ -100,49 +100,146 @@ $(document).ready(function () {
     $(".spikefeatures").hide();
     $(".deathfeatures").hide();
     $(".escalationfeatures").hide();
-    
-    $("#btn-modes").click(function () {
-        $(".unratedfeatures").hide();
-        $(".rankedfeatures").hide();
-        $(".spikefeatures").hide();
-        $(".deathfeatures").hide();
-        $(".escalationfeatures").hide();
-    });
 
     $("#btn-unratedfeatures").click(function () {
-        $(".unratedfeatures").show();
+        if ($('.rankedfeatures').is(":visible")) {
+            $(".unratedfeatures").show();
+        }
+        if ($('.spikefeatures').is(":visible")) {
+            $(".unratedfeatures").show();
+        }
+        if ($('.deathfeatures').is(":visible")) {
+            $(".unratedfeatures").show();
+        }
+        if ($('.escalationfeatures').is(":visible")) {
+            $(".unratedfeatures").show();
+        }
         $(".rankedfeatures").hide();
         $(".spikefeatures").hide();
         $(".deathfeatures").hide();
         $(".escalationfeatures").hide();
     });
     $("#btn-rankedfeatures").click(function () {
+        if ($('.unratedfeatures').is(":visible")) {
+            $(".rankedfeatures").show();
+        }
+        if ($('.spikefeatures').is(":visible")) {
+            $(".rankedfeatures").show();
+        }
+        if ($('.deathfeatures').is(":visible")) {
+            $(".rankedfeatures").show();
+        }
+        if ($('.escalationfeatures').is(":visible")) {
+            $(".rankedfeatures").show();
+        }
         $(".unratedfeatures").hide();
-        $(".rankedfeatures").show();
         $(".spikefeatures").hide();
         $(".deathfeatures").hide();
         $(".escalationfeatures").hide();
     });
     $("#btn-spikefeatures").click(function () {
+        if ($('.unratedfeatures').is(":visible")) {
+            $(".spikefeatures").show();
+        }
+        if ($('.rankedfeatures').is(":visible")) {
+            $(".spikefeatures").show();
+        }
+        if ($('.deathfeatures').is(":visible")) {
+            $(".spikefeatures").show();
+        }
+        if ($('.escalationfeatures').is(":visible")) {
+            $(".spikefeatures").show();
+        }
         $(".unratedfeatures").hide();
         $(".rankedfeatures").hide();
-        $(".spikefeatures").show();
         $(".deathfeatures").hide();
         $(".escalationfeatures").hide();
     });
     $("#btn-deathfeatures").click(function () {
+        if ($('.unratedfeatures').is(":visible")) {
+            $(".deathfeatures").show();
+        }
+        if ($('.rankedfeatures').is(":visible")) {
+            $(".deathfeatures").show();
+        }
+        if ($('.spikefeatures').is(":visible")) {
+            $(".deathfeatures").show();
+        }
+        if ($('.escalationfeatures').is(":visible")) {
+            $(".deathfeatures").show();
+        }
         $(".unratedfeatures").hide();
         $(".rankedfeatures").hide();
         $(".spikefeatures").hide();
-        $(".deathfeatures").show();
         $(".escalationfeatures").hide();
     });
     $("#btn-escalationfeatures").click(function () {
+        if ($('.unratedfeatures').is(":visible")) {
+            $(".escalationfeatures").show();
+        }
+        if ($('.rankedfeatures').is(":visible")) {
+            $(".escalationfeatures").show();
+        }
+        if ($('.spikefeatures').is(":visible")) {
+            $(".escalationfeatures").show();
+        }
+        if ($('.deathfeatures').is(":visible")) {
+            $(".escalationfeatures").show();
+        }
         $(".unratedfeatures").hide();
         $(".rankedfeatures").hide();
         $(".spikefeatures").hide();
         $(".deathfeatures").hide();
-        $(".escalationfeatures").show();
+    });
+
+    $("#btn-unratedfeatures1").click(function () {
+        $(".unratedfeatures").toggle();
+        $(".rankedfeatures").hide();
+        $(".spikefeatures").hide();
+        $(".deathfeatures").hide();
+        $(".escalationfeatures").hide();
+    });
+    $("#btn-rankedfeatures1").click(function () {
+        $(".unratedfeatures").hide();
+        $(".rankedfeatures").toggle();
+        $(".spikefeatures").hide();
+        $(".deathfeatures").hide();
+        $(".escalationfeatures").hide();
+    });
+    $("#btn-spikefeatures1").click(function () {
+        $(".unratedfeatures").hide();
+        $(".rankedfeatures").hide();
+        $(".spikefeatures").toggle();
+        $(".deathfeatures").hide();
+        $(".escalationfeatures").hide();
+    });
+    $("#btn-deathfeatures1").click(function () {
+        $(".unratedfeatures").hide();
+        $(".rankedfeatures").hide();
+        $(".spikefeatures").hide();
+        $(".deathfeatures").toggle();
+        $(".escalationfeatures").hide();
+    });
+    $("#btn-escalationfeatures1").click(function () {
+        $(".unratedfeatures").hide();
+        $(".rankedfeatures").hide();
+        $(".spikefeatures").hide();
+        $(".deathfeatures").hide();
+        $(".escalationfeatures").toggle();
     });
 });
 
+function change()
+{
+    var btnlist = ["btn-unratedfeatures1","btn-rankedfeatures1","btn-spikefeatures1","btn-deathfeatures1","btn-escalationfeatures1"]
+
+    for(i = 0; i < btnlist.length;i++){
+        var btn = document.getElementById(btnlist[i])
+        if (btn.innerHTML==="More Info"){
+            btn.innerHTML = "Less Info"
+        }
+        else if(btn.innerHTML ==="Less Info"){
+            btn.innerHTML = "More Info"
+        }
+    }
+}
