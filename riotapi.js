ba9a9c2d44b795b670e1870fe0ddbf49682c0037
Brian Cheung <44447718+BrianCheung1@users.noreply.github.com>
2021-03-09 21:19:28 -0500

    added leaderboard page

---
+++ riotapi.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+
+    let fetchBtn = document.getElementById("fetchBtn");
+
+    fetchBtn.addEventListener("click", buttonclickhandler);
+
+    function buttonclickhandler() {
+        // Instantiate an new XHR Object
+        const xhr = new XMLHttpRequest();
+
+        // Open an obejct (GET/POST, PATH,
+        // ASYN-TRUE/FALSE)
+        xhr.open("GET", "http://dummy.restapiexample.com/api/v1/employees", true);
+
+        // When response is ready
+        xhr.onload = function () {
+            if (this.status === 200) {
+                // Changing string data into JSON Object
+                obj = JSON.parse(this.responseText);
+
+                // Getting the ul element
+                let list = document.getElementById("list");
+                str = "";
+                for (key in obj.data) {
+                    console.log(obj.data[key])
+                    str += `<li>${obj.data[key].employee_name}</li><ul><li>Age: ${obj.data[key].employee_age}</li><li>Salary: ${obj.data[key].employee_salary}</li></ul>`;
+                }
+                list.innerHTML = str;
+            } else {
+                console.log("File not found");
+            }
+        };
+
+        // At last send the request
+        xhr.send();
+    }
+});
